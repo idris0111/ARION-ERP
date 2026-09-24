2d7e70341769da3104c06bf83e35510910780876
idris0111 <parvinaismatova794@gmail.com>
2026-09-24 20:06:06 +0500

    Add myapp serializer

---
+++ myapp/serializers.py
@@ -0,0 +1,241 @@
+from rest_framework import serializers
+from .models import (
+    Organization,
+    Branch,
+    OrganizationMember,
+    Department,
+    Position,
+    Employee,
+    SalaryPayment,
+    Counterparty,
+    ContactPerson,
+    Category,
+    Unit,
+    Brand,
+    Product,
+    PriceType,
+    ProductPrice,
+    Warehouse,
+    Stock,
+    StockMovement,
+    Purchase,
+    PurchaseItem,
+    Sale,
+    SaleItem,
+    StockTransfer,
+    StockTransferItem,
+    WriteOff,
+    WriteOffItem,
+    Inventory,
+    InventoryItem,
+    CashAccount,
+    FinanceCategory,
+    CashTransaction,
+    MoneyTransfer,
+    Debt,
+    AuditLog,
+)
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = '__all__'
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = '__all__'
+
+
+class OrganizationMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationMember
+        fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class SalaryPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalaryPayment
+        fields = '__all__'
+
+
+class CounterpartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Counterparty
+        fields = '__all__'
+
+
+class ContactPersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactPerson
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class UnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Unit
+        fields = '__all__'
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class PriceTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PriceType
+        fields = '__all__'
+
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPrice
+        fields = '__all__'
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = '__all__'
+
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'
+
+
+class StockMovementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockMovement
+        fields = '__all__'
+
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = '__all__'
+
+
+class PurchaseItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseItem
+        fields = '__all__'
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sale
+        fields = '__all__'
+
+
+class SaleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SaleItem
+        fields = '__all__'
+
+
+class StockTransferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockTransfer
+        fields = '__all__'
+
+
+class StockTransferItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockTransferItem
+        fields = '__all__'
+
+
+class WriteOffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WriteOff
+        fields = '__all__'
+
+
+class WriteOffItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WriteOffItem
+        fields = '__all__'
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class InventoryItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem
+        fields = '__all__'
+
+
+class CashAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CashAccount
+        fields = '__all__'
+
+
+class FinanceCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FinanceCategory
+        fields = '__all__'
+
+
+class CashTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CashTransaction
+        fields = '__all__'
+
+
+class MoneyTransferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoneyTransfer
+        fields = '__all__'
+
+
+class DebtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Debt
+        fields = '__all__'
+
+
+class AuditLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuditLog
+        fields = '__all__'
