ba2b5a20e0008a362ec0ad7258446d051b70185d
idris0111 <parvinaismatova794@gmail.com>
2026-09-24 19:56:01 +0500

    Add account amdin ponel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('ERP info', {
+            'fields': (
+                'role',
+                'phone',
+                'avatar',
+                'is_verified',
+            )
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('ERP info', {
+            'fields': (
+                'role',
+                'phone',
+            )
+        }),
+    )
+
+    list_display = [
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'role',
+        'phone',
+        'is_active',
+        'is_staff',
+    ]
+
+    list_filter = [
+        'role',
+        'is_active',
+        'is_staff',
+        'is_verified',
+    ]
+
+    search_fields = [
+        'username',
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+    ]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'position',
+        'hire_date',
+        'salary',
+        'is_employee',
+    ]
+
+    list_filter = [
+        'is_employee',
+        'position',
+    ]
+
+    search_fields = [
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'position',
+        'passport',
+    ]
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'myapp',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
