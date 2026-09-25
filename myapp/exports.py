afba6649212dfd64bbec3351bb1a7796135f2484
idris0111 <parvinaismatova794@gmail.com>
2026-09-25 17:36:16 +0500

    new failed exports.py

---
+++ myapp/exports.py
@@ -0,0 +1,40 @@
+import csv
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Product,Stock,Sale,Purchase,Debt
+
+def export_csv(queryset,filename):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{filename}.csv"'
+    writer = csv.writer(response)
+    fields = [field.name for field in queryset.model._meta.fields]
+    writer.writerow(fields)
+    for obj in queryset:
+        writer.writerow([getattr(obj,field) for field in fields])
+    return response
+
+class ExportProductsView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        return export_csv(Product.objects.all(),'products')
+
+class ExportStocksView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        return export_csv(Stock.objects.all(),'stocks')
+
+class ExportSalesView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        return export_csv(Sale.objects.all(),'sales')
+
+class ExportPurchasesView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        return export_csv(Purchase.objects.all(),'purchases')
+
+class ExportDebtsView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        return export_csv(Debt.objects.all(),'debts')
